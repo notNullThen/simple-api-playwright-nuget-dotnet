c5b05cd6554dccd1edb48cc1d8c77f054a6f39b0
notNullThen <epekaryuk@outlook.com>
2026-04-14 20:56:14 +0200

    page & context in ApiEndpointBase

--- SimpleApiPlaywright/Core/ApiEndpointBase.cs
+++ SimpleApiPlaywright/Core/ApiEndpointBase.cs
@@ -1,8 +1,14 @@
+using Microsoft.Playwright;
 using SimpleApiPlaywright.Core.Types;
 
 namespace SimpleApiPlaywright.Core;
 
-public abstract class ApiEndpointBase(string apiBaseUrl)
+public abstract class ApiEndpointBase(
+    string apiBaseUrl,
+    IPage? page = null,
+    IAPIRequestContext? context = null
+)
 {
-    public ApiAction<T> Action<T>(RequestParameters parameters) => new(apiBaseUrl, parameters);
+    public ApiAction<T> Action<T>(RequestParameters parameters) =>
+        new(apiBaseUrl, parameters, page, context);
 }
