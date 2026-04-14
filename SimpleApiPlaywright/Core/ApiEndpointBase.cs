ec4de22d4ce70c9a9194b7342fbd107bfacb3d05
notNullThen <epekaryuk@outlook.com>
2026-04-14 21:31:43 +0200

    format

--- SimpleApiPlaywright/Core/ApiEndpointBase.cs
+++ SimpleApiPlaywright/Core/ApiEndpointBase.cs
@@ -5,6 +5,5 @@ namespace SimpleApiPlaywright.Core;
 
 public abstract class ApiEndpointBase(string apiBaseUrl)
 {
-    public ApiAction<T> Action<T>(RequestParameters parameters) =>
-        new(apiBaseUrl, parameters);
+    public ApiAction<T> Action<T>(RequestParameters parameters) => new(apiBaseUrl, parameters);
 }
