a6283882b035aaaaa58b64569f121a38b1b939d9
notNullThen <epekaryuk@outlook.com>
2026-04-14 21:11:35 +0200

    ApiContext class

---
+++ SimpleApiPlaywright/Core/ApiContext.cs
@@ -0,0 +1,32 @@
+using Microsoft.Playwright;
+
+namespace SimpleApiPlaywright.Core;
+
+public class ApiContext
+{
+    private readonly IBrowserContext _browserContext;
+    private readonly IAPIRequestContext? _context;
+    private readonly IPage? _page;
+
+    public ApiContext(
+        IBrowserContext browserContext,
+        IPage? page = null,
+        IAPIRequestContext? context = null
+    )
+    {
+        if (page == null && context == null)
+        {
+            throw new PlaywrightException(
+                $"You need to provide at least '{nameof(page)}' or '{nameof(context)}' parameters to create an instance of '{nameof(ApiClient)}'."
+            );
+        }
+
+        _browserContext = browserContext;
+        _context = context ?? page!.APIRequest;
+        _page = page;
+    }
+
+    public IBrowserContext BrowserContext => _browserContext;
+    public IAPIRequestContext Context => _context!;
+    public IPage? Page => _page;
+}
